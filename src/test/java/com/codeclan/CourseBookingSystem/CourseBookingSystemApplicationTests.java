b105635b49bc95ee433a87b2022e4e0b6b0c73bc
CrugBarat <craigtbarratt@gmail.com>
2020-06-13 23:19:11 +0100

    adds tests canFindAll Courses/Customers/Bookings and tests routes in client - PASSED
    Signed-off-by: CrugBarat <craigtbarratt@gmail.com>

--- src/test/java/com/codeclan/CourseBookingSystem/CourseBookingSystemApplicationTests.java
+++ src/test/java/com/codeclan/CourseBookingSystem/CourseBookingSystemApplicationTests.java
@@ -1,13 +1,51 @@
 package com.codeclan.CourseBookingSystem;
 
+import com.codeclan.CourseBookingSystem.models.Booking;
+import com.codeclan.CourseBookingSystem.models.Course;
+import com.codeclan.CourseBookingSystem.models.Customer;
+import com.codeclan.CourseBookingSystem.repositories.BookingRepository;
+import com.codeclan.CourseBookingSystem.repositories.CourseRepository;
+import com.codeclan.CourseBookingSystem.repositories.CustomerRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class CourseBookingSystemApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Autowired
+	CourseRepository courseRepository;
+
+	@Autowired
+	BookingRepository bookingRepository;
+
+	@Autowired
+	CustomerRepository customerRepository;
+
+	@Test
+	public void canFindAllCourses(){
+		List<Course> found = courseRepository.findAll();
+		assertEquals(6, found.size());
+	}
+
+	@Test
+	public void canFindAllCustomers(){
+		List<Customer> found = customerRepository.findAll();
+		assertEquals(3, found.size());
+	}
+
+	@Test
+	public void canFindAllBookings(){
+		List<Booking> found = bookingRepository.findAll();
+		assertEquals(6, found.size());
 	}
 
 }
