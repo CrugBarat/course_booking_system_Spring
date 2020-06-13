eae63718f818a350b53a3aa3d66d44452722fd97
CrugBarat <craigtbarratt@gmail.com>
2020-06-13 21:42:17 +0100

    adds CustomerController with index route
    Signed-off-by: CrugBarat <craigtbarratt@gmail.com>

---
+++ src/main/java/com/codeclan/CourseBookingSystem/controllers/CustomerController.java
@@ -0,0 +1,20 @@
+package com.codeclan.CourseBookingSystem.controllers;
+import com.codeclan.CourseBookingSystem.repositories.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CustomerController {
+
+    @Autowired
+    CustomerRepository customerRepository;
+
+    @GetMapping(value = "/customers")
+    public ResponseEntity getCustomers(){
+        return new ResponseEntity(customerRepository.findAll(), HttpStatus.OK);
+    }
+
+}
